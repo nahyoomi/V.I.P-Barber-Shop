dde9e8222ab3da0af96fb36a5275844cf025edef
nahyoomi <nahyomi2000@gmail.com>
2023-04-11 21:46:28 +0100

    ADD :clown_face::Mock header

---
+++ BarberShop/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return ( 
+        <div className="header">
+            <div className="header--container">
+                <nav className="header--container--nav">
+                    <a href="#" className="nav--img">
+                        <img src="/" alt="logo"/>
+                    </a>
+                    <div className="nav--menu">
+
+                    </div>
+                </nav>
+            </div>
+        </div>
+    );
+}
+ 
+export default Header;
