0f04e20296bdda1811dc47d76bfb755be046240b
nahyoomi <nahyomi2000@gmail.com>
2023-04-11 23:24:14 +0100

    ADD ::truck: Move routes

--- BarberShop/src/main.tsx
+++ BarberShop/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from './routes/Root';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
